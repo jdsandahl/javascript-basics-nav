b5b8d8599a502a85a308b6436d4a938fd8c56b88
Justin Sandahl <sandahl.j.d@gmail.com>
2020-03-04 17:46:01 +0000

    completed booleans section, added comment sections in instances where alternative solutions were discovered

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,64 @@
 const negate = a => {
-  // your code here
+  return !a;
 };
 
 const both = (a, b) => {
-  // your code here
+  return a && b;
 };
 
 const either = (a, b) => {
-  // your code here
+  return a || b;
 };
 
 const none = (a, b) => {
-  // your code here
+  return !(a || b);
 };
 
 const one = (a, b) => {
-  // your code here
+  return (a && !b) || (!a && b);
 };
 
 const truthiness = a => {
-  // your code here
+  return !!a;
 };
 
 const isEqual = (a, b) => {
-  // your code here
+  return a === b;
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+  return a > b;
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+  return a <= b;
 };
 
 const isOdd = a => {
-  // your code here
+  return a % 2 !== 0;
 };
 
 const isEven = a => {
-  // your code here
+  return a % 2 === 0;
 };
 
 const isSquare = a => {
-  // your code here
+  return a >= 0 && Math.sqrt(a) % 1 === 0;
 };
 
 const startsWith = (char, string) => {
-  // your code here
+  return string.charAt(0) === char;
 };
 
 const containsVowels = string => {
-  // your code here
+  return /[aeiou]/.test(string.toLowerCase()); //RegEx way of doing it
+  /*
+  return string.split("").some(letter => ["a", "e", "i", "o", "u"].includes(letter.toLowerCase()));
+  */
 };
 
 const isLowerCase = string => {
-  // your code here
+  return string === string.toLowerCase();
 };
 
 module.exports = {
